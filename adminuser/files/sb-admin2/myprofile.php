4bd11ab1834c08701d0bd691d46640bbc2c1b9c7
muchammadbadrudin <93694877+muchammadbadrudin@users.noreply.github.com>
2022-02-16 08:39:18 +0700

    Update Email

--- adminuser/files/sb-admin2/myprofile.php
+++ adminuser/files/sb-admin2/myprofile.php
@@ -232,7 +232,7 @@
 											<tr>
 												<td>Email</td>
 												<td>:</td>
-												<td>freedomcode.sch.id</td>
+												<td>asta@smkn8malang.sch.id</td>
 											</tr>
 									    </tbody>
 									</table>
