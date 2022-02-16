986fe7f17a241885a3ec734e434604911072dae4
Zidaneyoga <93694885+Zidaneyoga@users.noreply.github.com>
2022-02-16 14:59:37 +0700

    up

--- adminuser/files/sb-admin2/myprofile.php
+++ adminuser/files/sb-admin2/myprofile.php
@@ -287,13 +287,13 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <<!-- Logout Modal-->
+    <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Yakin Ingin Pergi?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Yakin Ingin Keluar?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
